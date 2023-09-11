37fb3a1bda28c4fc580daba8f156bf1458bc2c19
wnesbv <wnesbv@protonmail.com>
2023-09-11 15:32:39 +0300

    first commit

--- account/urls.py
+++ account/urls.py
@@ -21,65 +21,55 @@ from .recovery import (
 routes = [
     Route(
         "/register",
-        endpoint=user_register,
+        user_register,
         methods=["GET", "POST", "OPTIONS"],
-        name="user__register",
     ),
     Route(
         "/list",
-        endpoint=user_list,
+        user_list,
         methods=["GET"],
-        name="user__list",
     ),
     Route(
         "/details/{id:int}",
-        endpoint=user_detail,
+        user_detail,
         methods=["GET"],
-        name="user__details",
     ),
     Route(
         "/update/{id:int}",
-        endpoint=user_update,
+        user_update,
         methods=["GET", "POST", "OPTIONS"],
-        name="user__update",
     ),
     Route(
         "/login",
-        endpoint=user_login,
+        user_login,
         methods=["GET", "POST", "OPTIONS"],
-        name="user_login",
     ),
     Route(
         "/logout",
-        endpoint=user_logout,
+        user_logout,
         methods=["GET", "POST", "OPTIONS"],
-        name="user_logout",
     ),
     # ...
     Route(
         "/email-verify/",
-        endpoint=verify_email,
+        verify_email,
         methods=["GET"],
-        name="email_verify",
     ),
     Route(
         "/email-verify-resend/",
-        endpoint=resend_email,
+        resend_email,
         methods=["GET", "POST", "OPTIONS"],
-        name="email_verify_resend",
     ),
     # ...
     Route(
         "/reset-password/",
-        endpoint=reset_password,
+        reset_password,
         methods=["GET", "POST"],
-        name="reset_password",
     ),
     Route(
         "/reset-password-confirm/",
-        endpoint=reset_password_confirm,
+        reset_password_confirm,
         methods=["GET", "POST"],
-        name="reset_password_confirm",
     ),
     Route(
         "/delete/{id:int}",
